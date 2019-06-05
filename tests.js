80774fefcac963390981e8288363a9c022456d3d
imperative-robot-4943 <drccamp32@gmail.com>
2019-06-05 08:13:19 +0000

    Automatically backed up by Learn

--- tests.js
+++ tests.js
@@ -1,6 +1,6 @@
 var name = "Susan"
 
-if (index.name === "Susan") {
+if (index.js name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected: Susan, Received: "+index.name)
